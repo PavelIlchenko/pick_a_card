88c3bea5031f4bf3e9a82a5168e3c27a57118df6
PavelIlchenko <ilchenkopi83@mail.ru>
2017-05-21 12:13:36 +0700

    Update pick_a_card.rb

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,6 +1,6 @@
 #encoding UTF-8
 
-values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
+values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'тотлолтотол]
 suits = ['Diamonds','Hearts','Clubs','Spades']
 
 puts "#{values.sample} of #{suits.sample}"
